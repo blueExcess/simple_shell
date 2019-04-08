ea9743152084767dc2eb1c04bafb3b0ce173e7cb
OrangeB0lt <667@holbertonschool.com>
2019-04-08 19:48:06 -0400

    added git.command as a global

--- parser.c
+++ parser.c
@@ -7,7 +7,6 @@ char **parser(char *string, char *delim)
 	unsigned int wrdCnt, index;
 	char **tokened, *token;
 
-
 	wrdCnt = _wordCount(string);
 	tokened = malloc((wrdCnt + 1) * sizeof(char *));
 	if (!tokened)
@@ -21,5 +20,36 @@ char **parser(char *string, char *delim)
 	{
 		tokened[index] = token = strtok('\0', delim);
 	}
+	global.command = tokened[0];
 	return (tokened);
+}
+
+/*fork/exec function*/
+void forkExec(char *argv[], char *path, char **env)
+{
+	pid_t child = 0;
+	char *fullPath;
+
+	fullPath = "bin/ls"; /*nate to make*/ /*search(argv[0], path);*/
+	if (!fullPath)
+	{
+		perror("command not found\n");
+		return;
+	}
+
+	switch (child = fork())
+	{
+	case -1:
+		perror("fork failed\n");
+		break;
+	case 0:
+		if (execve(fullPath, argv, env) == -1)
+			perror("Execution failed\n");
+		break;
+	default:
+		free(fullPath);
+		if (wait(NULL) == -1)
+			perror("wait failed\n");
+		break;
+	}
 }
