56471ac8e41dbb56a275a740324248cfe9ae90d9
OrangeB0lt <667@holbertonschool.com>
2019-04-13 19:04:42 -0400

    Add beginning of cd function

--- builtins.c
+++ builtins.c
@@ -1,9 +1,46 @@
-/* file for some of the builtins */
+#include "simpleshell.h"
 
-#include "shell.h"
-
-int history()
+void _cd(char **tokens)
 {
-	int i;
+	int index = 0;
+	char *newPath;
+	char buffer[2048];
 
-	for (i = 0; i
+	while (tokens[index++] != '\0')
+		;
+	if (index < 2)
+	{
+		newPath = cpyPath("HOME=");
+		getcwd(buffer, sizeof(cwd));
+		updateDir(buffer, "OLDPWD=");
+		if (chdir(newPath) != 0)
+		{
+			perror("");
+		}
+		getcwd(buffer, sizeof(buffer));
+		updateDir(buffer, "PWD=");
+	}
+	else
+	{
+		if (args[1][0] == '-')
+		{
+			newPath = cpyPath("OLDPWD=");
+			getcwd(buffer, sizeof(cwd));
+			updateDir(buffer, "OLDPWD=");
+			if (chdir(newPath) != 0)
+				perror("");
+			getcwd(buffer, sizeof(buffer));
+			updateDir(buffer, "PWD=");
+		}
+		else
+		{
+			getcwd(buffer, sizeof(buffer));
+			updateDir(buffer, "OLDPWD=");
+			if (chdir(tokens[1] != 0))
+				perror("");
+			getcwd(buffer, sizeof(buffer));
+			updateDir(buffer, "PWD=");
+		}
+	}
+	exit (0);
+}
